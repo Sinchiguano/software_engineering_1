9e43f50c3ee862e1b99b6626130af8ba53728a5a
CESAR SINCHIGUANO <cesarsinchiguano@hotmail.es>
2022-06-05 05:24:07 -0500

    factorialExample

--- crash_course_on_python/factorialExample.py
+++ crash_course_on_python/factorialExample.py
@@ -6,3 +6,8 @@ for i in range(1,6):
 	fact=fact*i
 	#pass
 print("The factorial of " +str(number)+" is: "+str(fact))
+
+
+'''
+git push https://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@github.com/Sinchiguano/software_engineering_1.git
+
